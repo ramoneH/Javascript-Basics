7d5e7e763dc2875e479ca75cf5ec8ea093801f84
Ramone Hamlett <ramone@Snapits-MacBook-Pro.local>
2018-01-30 16:29:48 -0600

    update.

---
+++ FizzyArrayPAC.js
@@ -0,0 +1,14 @@
+
+numarray(7)
+numarray(3)
+
+resultArray = numarray(10) ;
+
+function numarray(n) {
+    resultArray = Array[n]
+    for (w = 0; w < n; w++) {
+        resultArray[w] === w ;
+    return resultArray;
+    }
+    console.log(resultArray);
+}
