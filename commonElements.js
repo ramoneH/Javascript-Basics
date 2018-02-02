09300825e99034a91ed88c9ab2d525ca45341db8
Ramone Hamlett <ramone@Snapits-MacBook-Pro.local>
2018-02-02 16:31:08 -0600

    last of all excercises

--- commonElements.js
+++ commonElements.js
@@ -5,18 +5,21 @@
 
 vowelCount();
 
+
 function vowelCount() {
     var arr1 = [4, 7, 3, 9, 2];
     var arr2 = [3, 2, 12, 9, 40, 32, 4];
+    console.log(arr2);
+    console.log(arr1);
     for (var i = 0; i < arr1.length; i++) {
         for (var j = 0; j < arr2.length; j++) {
             if (arr1[i] === arr2[j]) {
                var test = arr2[j];
+               console.log("Common Numers In BOTH Arrays :" + test);
             }
         }
     }
-    console.log(arr2)
-    console.log(arr1)
-    console.log("Common Numers In BOTH Arrays :" + test)
+    
+    
     
 }
